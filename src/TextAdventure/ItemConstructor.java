aa54da7119e9ac96b5068960afa0ac905a339561
bo_ma <bo_ma@10.50.137.33>
2017-04-04 19:27:34 +0200

    Tilføjet et par armor og potions.

--- src/TextAdventure/ItemConstructor.java
+++ src/TextAdventure/ItemConstructor.java
@@ -17,8 +17,16 @@ public class ItemConstructor {
     
     public void createItems() {
         w1 = new Weapon("Dagger",100,10);
-        a1 = new Armor("Vest",50,15);
+        a1 = new Armor("Leather vest from the 90's",50,15);
+        a1 = new Armor("Chestpiece of invisible torso", 10, 2);
+        a1 = new Armor("Cursed bone kilt from a slut", 100, 20);
+        a1 = new Armor("Sandal of twisted hell", 40,40);
+        a1 = new Armor("Unded wrap of darkness", 60, 10);
         p1 = new Potion("Redbull",2,2);
+        p1 = new Potion("Brew of better java writing",10, 50);
+        p1 = new Potion("Potion of burning hot chocolate",-10,4);
+        p1 = new Potion("A viel of whiskey tornado", 40, 5);
+        
     
     }
     
