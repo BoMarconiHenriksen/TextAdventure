8ccb94981b9362ffd89f994e30f73086e772d29e
AndersHC <AndersHC@LAPTOP-494L3SH8>
2017-04-11 13:05:50 +0200

    indsat combat menu i combat loop. Samt muligheder for hvad der sker mm.        Error: Combat kender ikke Potion

--- src/TextAdventure/Combat.java
+++ src/TextAdventure/Combat.java
@@ -4,14 +4,17 @@ package TextAdventure;
  *  Klassen der afvikler combat mellem player og npc.
  * @since 3.0
  */
+
 public class Combat {
 
-
+    
     public void combat(NPC npc, Player player, Display display) {
         
         boolean combatLoop = true;
         boolean npcTurn = true;
         boolean playerTurn = true;
+       
+        
 
         while (combatLoop) {
             while (npcTurn) {
@@ -26,8 +29,41 @@ public class Combat {
             }
 
             while (playerTurn) {
-                player.doAttack(npc);
-                display.npcHealthStatus(npc);
+                
+                switch(display.combatMenu()){
+                    case 1: 
+                            player.doAttack(npc);
+                            display.npcHealthStatus(npc);
+                            break;
+                    case 2:
+                           if(player.inventory.getItem(3, 1)== null){
+                               display.insufficientAmount();
+                               
+                           }else{
+                               display.usePotion();
+                               int potionEffect = potion.getRandomUseEffect();
+                               
+                               if(potionEffect => 0){
+                                   player.stats.setHealth(player.stats.getHealth()+potionEffect);
+                                   display.gainLife(potionEffect);
+                                   
+                               }else{
+                                   player.stats.setHealth(player.stats.getHealth()+potionEffect);
+                                   display.takeDamage(potionEffect, player);
+                                   
+                               }
+                           }
+                           display.playerHealthStatus(player);
+                        break;
+                        
+                    case 3:
+                           // player.run
+                           // display.runattempt
+                           // if succes, combatloop = false
+                        break;
+                   
+                }
+                
                 playerTurn = false;
                 if (npc.stats.getHealth() <= 0) {
                     display.npcDied();
