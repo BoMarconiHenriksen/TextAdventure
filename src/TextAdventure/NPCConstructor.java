110cc1f4c56c4e360b654550e143a8cefe65b010
AndersHC <AndersHC@LAPTOP-494L3SH8>
2017-04-19 11:37:07 +0200

    combat fixed

--- src/TextAdventure/NPCConstructor.java
+++ src/TextAdventure/NPCConstructor.java
@@ -5,7 +5,7 @@ package TextAdventure;
  *  @since 3.0
  */
 public class NPCConstructor implements java.io.Serializable{
-    
+ 
     NPC nmy1;
     NPC nmy2;
     NPC nmy3;
@@ -17,7 +17,7 @@ public class NPCConstructor implements java.io.Serializable{
      */
     public void createNPCS() {
         nmy1 = new NPC("Spooky Scary Skeleton",new Stats(30,10,3),new Equipped(),new Inventory());
-        nmy2 = new NPC("Fat Goblin",new Stats(40,12,4),new Equipped(),new Inventory());
+        nmy2 = new NPC("Fat Goblin",new Stats(40,4,5),new Equipped(),new Inventory());
         nmy3 = new NPC("Giant Snek",new Stats(60,8,10),new Equipped(),new Inventory());
         nmy4 = new NPC("Mysterious Gentleman",new Stats(80,15,5),new Equipped(),new Inventory());
         nmy5 = new NPC("The Guardian",new Stats(100,15,15),new Equipped(),new Inventory());
