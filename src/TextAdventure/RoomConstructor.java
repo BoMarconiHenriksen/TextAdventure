665a3ae98d248c322932745a47500d72eafa8888
AndersHC <AndersHC@LAPTOP-494L3SH8>
2017-04-20 14:14:25 +0200

    Ny map. Descriptions er default

--- src/TextAdventure/RoomConstructor.java
+++ src/TextAdventure/RoomConstructor.java
@@ -15,6 +15,11 @@ public class RoomConstructor implements java.io.Serializable {
     Room room6;
     Room room7;
     Room room8;
+    Room room9;
+    Room room10;
+    Room room11;
+    Room room12;
+    Room room13;
     Room slutRoom;
     Room trapRoom1;
     Room trapRoom2;
@@ -25,34 +30,33 @@ public class RoomConstructor implements java.io.Serializable {
     */
     public void createRooms() {
         
-        startRoom = new Room(new Inventory(),"The room is moist and a brown glow emanates from the walls." 
-                                +"\nIt has three doorways; One to the north, one to the east, and one to the south.",false);
+        startRoom = new Room(new Inventory(),"This is the startroom",false);
         
-        room1 = new Room(new Inventory(),"The room is long and narrow, with rays of light shining through cracks in the roof.\n" 
-                                +"It has a dark tunnel to the east and a doorway to the south and west.");
+        room1 = new Room(new Inventory(),"This is room 1");
         
-        room2 = new Room(new Inventory(),"The room is long and dark, and you can barely see the walls around you.\n"
-                                +"It has a pulsating portal to the west and a doorway to the east.");
+        room2 = new Room(new Inventory(),"This is room 2");
         
-        room3 = new Room(new Inventory(),"The room has a stony floor, and dripping crystals hanging from the celing.\n"
-                                +"It has three doorways; One to the north, one to the west, and one to the south.");
+        room3 = new Room(new Inventory(),"This is room 3");
         
-        room4 = new Room(new Inventory(),"The room is a dead end.\n"
-                                +"Your only choice is to go back.");
+        room4 = new Room(new Inventory(),"This is room 4");
         
-        room5 = new Room(new Inventory(),"The room is narrow and blocked by rocks. You can barely move through.\n"
-                                +"It has a dark tunnel to the south and a doorway to the west. ");
+        // room 5 is a trap room
         
-        room6 = new Room(new Inventory(),"The room is a wide tunnel, with strange colours coming from the cracks in the floor.\n"
-                                +"It has a doorway to the north and a doorway to the south.");
+        room6 = new Room(new Inventory(),"This is room 6");
         
-        room7 = new Room(new Inventory(),"The room is a huge circle, with odd, flickering colours of red coming from the walls. "
-                                + "It has a pulsating portal to the west and a doorway to the south.");
+        room7 = new Room(new Inventory(),"This is room 7");
         
-        room8 = new Room(new Inventory(),"The room is start-shaped, well lit from floating lights, calmly orbiting the ground.\n"
-                                +"It has a pulsating portal to the west and a sturdy looking gate to the east.\n"
-                                +"The gate has a stone tablet above it, with crude letters carved into it.\n"
-                                +"It read: \"My treasure you must collect, in its entirety. Only then can you escape this domain\"");
+        room8 = new Room(new Inventory(),"This is room 8");
+        
+        room9 = new Room(new Inventory(),"This is room 9");
+        
+        // room 10 is a trap room
+        
+        room11 = new Room(new Inventory(),"This is room 11");
+        
+        room12 = new Room(new Inventory(),"This is room 12");
+        
+        room13 = new Room(new Inventory(),"This is room 13");
         
         slutRoom = new Room(new Inventory(),"A ray of sunlight momentarilly blind you. "
                             + "\nAs you stagger out of the darkness, you hear birds singing, smell the fresh air, feel the grass beneath your feet - you finally escaped the dungeon! \n"
@@ -67,50 +71,67 @@ public class RoomConstructor implements java.io.Serializable {
                                 + "\nYour only choice is to go back.",true);
         
         //Opretter Exits start room
-        startRoom.setExitNorth(new Exit(room3));
-        startRoom.setExitEast(new Exit(room1));
-        startRoom.setExitSouth(new Exit(trapRoom2));
+        startRoom.setExitSouth(new Exit(room3));
 
         //Opretter Exits room1
-        room1.setExitWest(new Exit(startRoom));
-        room1.setExitEast(new Exit(room5));
-        room1.setExitSouth(new Exit(trapRoom1));
+        room1.setExitEast(new Exit(room2));
+      
         
         //Opretter Exits room2
         room2.setExitEast(new Exit(room3));
-        room2.setExitWest(new Exit(room8));
+        room2.setExitWest(new Exit(room1));
+        room2.setExitSouth(new Exit(room6));
 
         //Opretter Exits room3
         room3.setExitWest(new Exit(room2));
-        room3.setExitSouth(new Exit(startRoom));
-        room3.setExitNorth(new Exit(room6));
+        room3.setExitEast(new Exit(room4));
+        
         
         //Opretter Exits room4
-        room4.setExitEast(new Exit(room5));
+        room4.setExitEast(new Exit(trapRoom1));
+        room4.setExitWest(new Exit(room3));
+        room4.setExitSouth(new Exit(room8));
         
-        //Opretter Exits room5
-        room5.setExitWest(new Exit(room4));
-        room5.setExitSouth(new Exit(room1));
         
         //Opretter Exits room6
-        room6.setExitSouth(new Exit(room3));
-        room6.setExitNorth(new Exit(room7));
+        room6.setExitEast(new Exit(room7));
+        room6.setExitNorth(new Exit(room2));
         
         //Opretter Exits room7
-        room7.setExitSouth(new Exit(room6));
-        room7.setExitWest(new Exit(room8));
+        room7.setExitSouth(new Exit(room9));
+        room7.setExitWest(new Exit(room6));
+        room7.setExitEast(new Exit(room8));
         
         //Opretter Exits room8
-        room8.setExitEast(new Exit(slutRoom));
-        room8.setExitWest(new Exit(startRoom));
-        //Opretter Lås for Exit mod øst
-        room8.getExitEast().setOpen(false);
+        room8.setExitNorth(new Exit(room4));
+        room8.setExitWest(new Exit(room7));
+        
+        //Opretter Exits room9
+        room9.setExitNorth(new Exit(room7));
+        room9.setExitSouth(new Exit(room11));
+        
+       
+        
+        //Opretter Exits room11
+        room11.setExitNorth(new Exit(room9));
+        room11.setExitSouth(new Exit(room13));
+        room11.setExitEast(new Exit(room12));
+        room11.setExitWest(new Exit(trapRoom2));
+        
+        //Opretter Exits room12
+        room12.setExitWest(new Exit(room11));
+        
+        //Opretter Exits room13
+        room13.setExitNorth(new Exit(room11));
+        room13.setExitSouth(new Exit(slutRoom));
+
+ 
         
         //Opretter Exits trap room1
-        trapRoom1.setExitNorth(new Exit(room1));
+        trapRoom1.setExitWest(new Exit(room4));
         
         //Opretter Exits trap room2
-        trapRoom2.setExitNorth(new Exit(startRoom));
+        trapRoom2.setExitEast(new Exit(room11));
     }
     
     
