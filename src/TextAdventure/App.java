b4621df93ee87f466d72093e6cc49645234ed257
bo_ma <bo_ma@10.50.137.33>
2017-04-21 16:07:06 +0200

    tilføjet package-info.java fil til hver package og slettet html filen

--- src/TextAdventure/App.java
+++ src/TextAdventure/App.java
@@ -1,30 +1,3 @@
-/**
-* <h1>Text Adventure Game</h1>
-* Analysis, design and implementation of a textual adventure game (TAG). A TAG
-* is a dungeon described in text only.
-* 
-* <h2>Single player dungeon</h2>
-* We have designed and implement an application that create a small dungeon, 
-* single player dungeon game. There is no graphics handling.
-* 
-* <h2>The game</h2>
-* The player can walk and some places portal through the dungeon by issuing 
-* text commands. Its possible to find gold, items, potions and armor, and you 
-* can encounter traps in the rooms.
-* 
-* <h2>Version</h2>
-* Version 1.0 = milestone 1.
-* Version 2.0 = milestone 2.
-* Version 3.0 = milestone 3.
-* 
-* @author Alexander W. Hørsted-Andersen
-* @author Anders Hedegaard Christiansen
-* @author Mikkel Emil Larsen
-* @author Bo Henriksen
-* 
-* @version 2.0
-* 
-*/
 package TextAdventure;
 
 import Controllers.MainController;
