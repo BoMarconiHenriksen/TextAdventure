9ac65bd976b8b7832e9ab09dbc8d985902d45f68
AndersHC <AndersHC@LAPTOP-494L3SH8>
2017-04-21 08:47:27 +0200

    Balancing

--- src/TextAdventure/NPCConstructor.java
+++ src/TextAdventure/NPCConstructor.java
@@ -16,11 +16,11 @@ public class NPCConstructor implements java.io.Serializable{
      *
      */
     public void createNPCS() {
-        nmy1 = new NPC("Spooky Scary Skeleton",new Stats(30,10,3),new Equipped(),new Inventory());
-        nmy2 = new NPC("Fat Goblin",new Stats(40,4,5),new Equipped(),new Inventory());
-        nmy3 = new NPC("Giant Snek",new Stats(60,8,10),new Equipped(),new Inventory());
-        nmy4 = new NPC("Mysterious Gentleman",new Stats(80,15,5),new Equipped(),new Inventory());
-        nmy5 = new NPC("The Guardian",new Stats(100,15,15),new Equipped(),new Inventory());
+        nmy1 = new NPC("Spooky Scary Skeleton",new Stats(30,5,5),new Equipped(),new Inventory());
+        nmy2 = new NPC("Crazy maniac",new Stats(40,10,5),new Equipped(),new Inventory());
+        nmy3 = new NPC("Giant ghost Snek",new Stats(60,5,10),new Equipped(),new Inventory());
+        nmy4 = new NPC("Mysterious Gentleman",new Stats(80,10,5),new Equipped(),new Inventory());
+        nmy5 = new NPC("The Guardian",new Stats(100,10,15),new Equipped(),new Inventory());
     }
     
 }
