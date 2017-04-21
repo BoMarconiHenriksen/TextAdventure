94cd2431d9d4b81b7e797b5ba1c2a5187fe893ee
AndersHC <AndersHC@LAPTOP-494L3SH8>
2017-04-21 09:11:35 +0200

    Dungeon design  minus monsters..

--- src/TextAdventure/DungeonConstructor.java
+++ src/TextAdventure/DungeonConstructor.java
@@ -22,13 +22,61 @@ public class DungeonConstructor implements java.io.Serializable{
         ic.createItems();
         npc.createNPCS();
         
+        // Adding items to rooms
+        
         rc.startRoom.getInventory().addItem(2, ic.a1);
         rc.startRoom.getInventory().addItem(1, ic.w1);
         rc.startRoom.getInventory().addItem(3, ic.p1);
-        
         rc.startRoom.getInventory().getGoldList().get(0).setAmount(8);
+        //-------------------------------------------------------------------
         
-        //ADD SKELETOR TO STARTROOM
+        rc.room1.getInventory().getGoldList().get(0).setAmount(15);
+        
+        
+        rc.room2.getInventory().addItem(2, ic.a1);
+        
+        
+        rc.room3.getInventory().addItem(1, ic.w1);
+        
+        
+        rc.room4.getInventory().addItem(3, ic.p1);
+        
+        
+        rc.trapRoom1.getInventory().getGoldList().get(0).setAmount(10);
+        
+        
+        rc.room6.getInventory().addItem(3, ic.p1);
+        
+       
+        rc.room7.getInventory().addItem(1, ic.w2);
+        
+        
+        rc.room8.getInventory().getGoldList().get(0).setAmount(10);
+        
+        
+        rc.room9.getInventory().getGoldList().get(0).setAmount(20);
+        rc.room9.getInventory().addItem(3, ic.p1);
+        
+        
+        rc.trapRoom2.getInventory().getGoldList().get(0).setAmount(20);
+        rc.trapRoom2.getInventory().addItem(1, ic.w4);
+        
+        
+        rc.room11.getInventory().addItem(2, ic.a3);
+        
+        
+        rc.room12.getInventory().getGoldList().get(0).setAmount(10);
+        
+        
+        rc.room13.getInventory().getGoldList().get(0).setAmount(15);
+        
+        
+        
+        
+        
+        
+        
+        // Adding NPCs to rooms
         rc.startRoom.setNpc(npc.nmy1);
         
         
